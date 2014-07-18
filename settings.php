aa13a036d7f9692973899710253ace2ac3fad0a2
3i Logic LMS <azmat@3ilogic.com>
2014-07-18 15:24:32 +0500

    Update settings.php
    Added functionality to received SMS having different role & new role option to removed blank drop down role issue.
    (Special thanks to  mattmoodle to contribute the code)

--- settings.php
+++ settings.php
@@ -1,4 +1,5 @@
 <?php
+
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -19,23 +20,47 @@
  * send text messages to their student and teacher.
  * @package blocks
  * @author: Azmat Ullah, Talha Noor
- * @date: 06-Jun-2013
-*/
+ * @date: 17-Jul-2014
+ */
 
 defined('MOODLE_INTERNAL') || die;
-if($ADMIN->fulltree) {
-    $settings->add(new admin_setting_configtext(get_string('block_sms_apikey', 'block_sms'),
-                                                        get_string('sms_api_key', 'block_sms'),
-                                                        get_string('sms_api_key', 'block_sms'),
-                                                        '', PARAM_TEXT));
-    $settings->add(new admin_setting_configtext(get_string('block_sms_api_username', 'block_sms'),
-                                                        get_string('sms_api_username', 'block_sms'),
-                                                        get_string('sms_api_username', 'block_sms'),
-                                                        '', PARAM_TEXT));
-    $settings->add(new admin_setting_configtext(get_string('block_sms_api_password', 'block_sms'),
-                                                        get_string('sms_api_password', 'block_sms'),
-                                                        get_string('sms_api_password', 'block_sms'),
-                                                        '', PARAM_TEXT));
+if ($ADMIN->fulltree) {
+    // API SETTINGS
 
-    $settings->add(new admin_setting_configselect('block_sms_api','SMS API Name', 'Select Api which you are using', 'Sendsms.pk', array('Clickatell','Sendsms.pk')));
+    $settings->add(new admin_setting_heading(
+            'headerapi', 'API settings', 'Set up the API for SMS messaging'
+    ));
+
+    $settings->add(new admin_setting_configtext(get_string('block_sms_apikey', 'block_sms'), get_string('sms_api_key', 'block_sms'), get_string('sms_api_key', 'block_sms'), '', PARAM_TEXT));
+    $settings->add(new admin_setting_configtext(get_string('block_sms_api_username', 'block_sms'), get_string('sms_api_username', 'block_sms'), get_string('sms_api_username', 'block_sms'), '', PARAM_TEXT));
+    $settings->add(new admin_setting_configtext(get_string('block_sms_api_password', 'block_sms'), get_string('sms_api_password', 'block_sms'), get_string('sms_api_password', 'block_sms'), '', PARAM_TEXT));
+
+    $settings->add(new admin_setting_configselect('block_sms_api', 'SMS API Name', 'Select Api which you are using', 'Sendsms.pk', array('Clickatell', 'Sendsms.pk')));
+
+    // ROLE SETTINGS
+
+    $settings->add(new admin_setting_heading(
+            'headerrolesettings', 'Role settings', 'Define the users that can receive SMS'
+    ));
+
+
+    $settings->add(new admin_setting_configselect('block_sms_rolenames', 'Role names to use', 'Select Moodle role names which you want to use', 'Moodle short names', array('Moodle short names', 'Custom names', 'Archetypes')), 'Moodle short names');
+
+    //returns a list of role IDs separated by ','
+
+    $roles = $DB->get_records('role', null, 'sortorder ASC');
+
+    $default_sns = array('manager', 'editingteacher', 'teacher', 'student');
+    $defaults = array_filter($roles, function ($role) use ($default_sns) {
+        return in_array($role->shortname, $default_sns);
+    });
+
+    $only_names = function ($role) {
+        return $role->shortname;
+    };
+
+    $settings->add(
+            new admin_setting_configmultiselect('block_sms_roleselection', 'Select roles allowed to receive SMS', 'Select multiple roles holding CTRL key. Here roles are shown with their shortnames.', array_keys($defaults), array_map($only_names, $roles)
+            )
+    );
 }
