07c7fc76c359dcd450e8486939a921db1b5e42f9
3i Logic LMS <azmat@3ilogic.com>
2014-07-18 15:12:41 +0500

    Update block_sms.php
    Added new strings

--- lang/en/block_sms.php
+++ lang/en/block_sms.php
@@ -30,6 +30,90 @@
     $string['edithtml'] = 'Data Manipulation';
     $string['pagetitle'] = 'Input Text';
     $string['picturedesc'] = 'Description';
+
+moodle-block_sms / lang / en /
+
+or cancel
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+/* SMS Notifier Block
+ * SMS notification is a one way SMS messaging block that allows managers, teachers and administrators to
+ * send text messages to their student and teacher.
+ * @package blocks
+ * @author: Azmat Ullah, Talha Noor
+ * @date: 06-Jun-2013
+*/
+    $string['pluginname'] = 'SMS Notifier';
+    $string['sms'] = 'SMS Notifier';
+    $string['sms_send'] = 'Send SMS';
+    $string['addpage'] = 'Add Page';
+    $string['textfields'] = 'Data';
+    $string['edithtml'] = 'Data Manipulation';
+    $string['pagetitle'] = 'Input Text';
+    $string['picturedesc'] = 'Description';
+    $string['sms_save'] = 'Message Template';
+    $string['sms_history'] = 'Message History';
+    $string['selectcourse'] = 'Select Course';
+    $string['selectrole'] = 'Select Role';
+    $string['sms_body'] = 'Message Body';
+    $string['selectmsg'] = 'Select Message';
+    $string['template'] = 'Create Message Template';
+3i Logic LMS
+Commit changes
+
     $string['sms_save'] = 'Message Template';
     $string['sms_history'] = 'Message History';
     $string['selectcourse'] = 'Select Course';
@@ -47,3 +131,19 @@
     $string['block_sms_api_username'] = 'block_sms_api_username';
     $string['block_sms_api_password'] = 'block_sms_api_password';
     $string['sms:addinstance']='Add Instance';
+    $string['error'] = 'Error';
+    $string['sent'] = 'Sent';
+    $string['valid_key'] = 'Your key IS VALID';
+    $string['serial_no'] = 'Serial No.';
+    $string['name'] = 'Name';
+    $string['msg_body'] = 'Message Body';
+    $string['edit'] = 'Edit';
+    $string['delete'] = 'Delete';
+    $string['cell_no'] = 'Cell Number';
+    $string['select'] = 'Select';
+    $string['askfordelete'] = 'Do You Want to Delete This Template?';
+    $string['moodleuser'] = 'Username';
+    $string['usernumber'] = 'Numbers';
+    $string['status'] = 'Status';
+    $string['group_selection'] = 'Group Selection';
+    $string['group'] = 'Group';
