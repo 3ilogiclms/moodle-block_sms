a3cce4bd394b2a52a8891f599683904874607a82
3i Logic LMS <lms@3ilogic.com>
2015-07-11 15:50:06 +0500

    Remove warning in messag body
    Remove warning which occurs in message body when message template drop down selected null.

--- load_message.php
+++ load_message.php
@@ -29,5 +29,10 @@ require_once("lib.php");
 // Message ID.
 $m_id = required_param('m_id', PARAM_INT);
 global $DB;
-$result = $DB->get_record_sql('SELECT template  FROM {block_sms_template} where id=?', array($m_id));
+if($m_id!=0) {
+$result = $DB->get_record_sql('SELECT template  FROM {block_sms_template} where id=?', array($m_id)); }
+else {
+    $result=new stdClass();
+    $result->template = '-';
+}
 echo $result->template;
